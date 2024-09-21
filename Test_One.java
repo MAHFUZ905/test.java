c21249c4d68fcaea126ef1216e804582e48e72db
MAHFUZ AHMED <120004939+MAHFUZ905@users.noreply.github.com>
2024-09-21 23:29:00 +0600

    Update Test_One.java

--- Test_One.java
+++ Test_One.java
@@ -2,18 +2,15 @@ class Person {
     private String name;
     private int age;
 
-    //Make a default constructor
+    // Default constructor
     public Person() {
-        this.name="mahfuz";
-        this.age=28;
-
+        this.name = "mahfuz";
+        this.age = 28;
     }
 
-    //Make a Parameterized constructor
     public Person(String name, int age) {
-        //Write your code here...
-        this.name=name;
-        this.age=age;
+        this.name = name;
+        this.age = age;
     }
 
     // Getter methods
@@ -26,12 +23,11 @@ class Person {
     }
 }
 
-class ICE {
+public class ICE {
     public static void main(String[] args) {
-        person person1 = new person();
-        person person2 = new person(mahfuz,28);
-        
-        
+        Person person1 = new Person();
+        Person person2 = new Person("mahfuz", 28);
+
         System.out.println("Person 1 - Name: " + person1.getName() + ", Age: " + person1.getAge());
         System.out.println("Person 2 - Name: " + person2.getName() + ", Age: " + person2.getAge());
     }
